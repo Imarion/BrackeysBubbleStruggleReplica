35dfb3abc614428e40bfe0a5870913a926871e27
Manu <black_falcon00@hotmail.com>
2019-09-04 16:28:08 +0200

    Done

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Vector2 startForce;
+    public Rigidbody2D rb;
+
+    public GameObject nextBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(startForce, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Split()
+    {
+        if (nextBall != null)
+        {
+            GameObject ball1 = Instantiate(nextBall, rb.position + Vector2.right /4f, Quaternion.identity);
+            GameObject ball2 = Instantiate(nextBall, rb.position + Vector2.left / 4f, Quaternion.identity);
+
+            ball1.GetComponent<Ball>().startForce = new Vector2(2f, 5f);
+            ball2.GetComponent<Ball>().startForce = new Vector2(-2f, 5f);
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 4f;
+    public Rigidbody2D rb;
+
+    private float movement = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal") * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Vector2.right * movement * Time.fixedDeltaTime);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ball")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
